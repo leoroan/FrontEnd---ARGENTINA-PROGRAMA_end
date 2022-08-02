e47b385af7d1dee683000f2b575f5d4a02dc3132
Leandro M <leoroan@gmail.com>
2022-08-01 23:22:28 -0300

    correccion temas rutas

--- MiPortfolioAP/src/app/app-routing.module.ts
+++ MiPortfolioAP/src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { AcercadeComponent } from './acercade/acercade.component';
+
 const routes: Routes = [
-  //{ path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/acercade', pathMatch: 'full' },
+  { path: 'acercade', component: AcercadeComponent },
 ];
 
 @NgModule({
