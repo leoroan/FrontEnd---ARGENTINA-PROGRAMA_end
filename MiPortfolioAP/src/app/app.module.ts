d51c664c766f26ff6ade830ab9e10d67b34d357e
Leandro M <leoroan@gmail.com>
2022-08-01 22:47:21 -0300

    adden top & bottom bar

--- MiPortfolioAP/src/app/app.module.ts
+++ MiPortfolioAP/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { BottomBarComponent } from './bottom-bar/bottom-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    BottomBarComponent
   ],
   imports: [
     BrowserModule,
