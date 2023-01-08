2530824141a49fc80d667fc392f1dc7a6ad6af77
Leandro M <leoroan@gmail.com>
2023-01-08 13:22:35 -0300

    habildiades now can create new elements

--- MiPortfolioAP/src/app/hardysoft/edicion-crear-nuevo-habilidad/edicion-crear-nuevo-habilidad.component.ts
+++ MiPortfolioAP/src/app/hardysoft/edicion-crear-nuevo-habilidad/edicion-crear-nuevo-habilidad.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-edicion-crear-nuevo-habilidad',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EdicionCrearNuevoHabilidadComponent implements OnInit {
 
-  constructor() { }
+  someInput: any;
+  formdata: any;
+  @Output() createEvent = new EventEmitter();
+
+  constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
+    this.formdata = new FormGroup({
+      nombre_habilidad: new FormControl( '', [ Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
+      porcentaje: new FormControl('', [Validators.required, Validators.min(0), Validators.max(100)]),
+      priodo: new FormControl('mm/aaaa', Validators.required),
+      descripcion: new FormControl('', [Validators.required, Validators.maxLength(250)])
+    });
+
+  }
+
+  onClickSubmit(datos: any) {
+    this.datosPortfolio.agregarActualizarDatosHabilidades(datos)
+      .subscribe(data => {
+        console.log(JSON.stringify(datos));
+        this.createEvent.emit();
+      })
+    this.formdata.reset();
   }
 
 }
