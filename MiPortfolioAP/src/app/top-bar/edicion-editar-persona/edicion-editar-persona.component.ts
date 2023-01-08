de17027501269d0da7024a6f890402269cfa2768
Leandro M <leoroan@gmail.com>
2023-01-08 14:48:13 -0300

    agregado los datos de la bdd a la admin del perfil

--- MiPortfolioAP/src/app/top-bar/edicion-editar-persona/edicion-editar-persona.component.ts
+++ MiPortfolioAP/src/app/top-bar/edicion-editar-persona/edicion-editar-persona.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
@@ -9,27 +9,32 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
 })
 export class EdicionEditarPersonaComponent implements OnInit {
 
+  @Input() thisPer: any;
   someInput: any;
   formdata: any;
   mailRegex = /@.*[a-zA-Z]+/;
-  urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+  urlRegex = /w+\..*\.[A-Za-z0-9]+/ism;
   @Output() createEvent = new EventEmitter();
 
   constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
     this.formdata = new FormGroup({
-      nombre: new FormControl( '', [ Validators.required, Validators.minLength(10), Validators.maxLength(75)]),
-      apellido: new FormControl('', [Validators.required, Validators.minLength(10), Validators.maxLength(75)]),
-      email: new FormControl('', [Validators.required, Validators.pattern(this.mailRegex)]),
-      fecha_nac: new FormControl('dd/mm/aaaa', [Validators.required, Validators.maxLength(10)]),
-      titulo_actual: new FormControl('', [Validators.required, Validators.maxLength(250)]),
-      sobre_mi: new FormControl('', [Validators.required, Validators.maxLength(250)]),
-      contrasenia: new FormControl('', [Validators.required, Validators.maxLength(10)]),
-      linkedin: new FormControl('', [Validators.required, Validators.maxLength(250), Validators.pattern(this.urlRegex)]),
-      facebook: new FormControl('', [Validators.required, Validators.maxLength(250),Validators.pattern(this.urlRegex)]),
-      github: new FormControl('', [Validators.required, Validators.maxLength(250), Validators.pattern(this.urlRegex)]),
+      nombre: new FormControl( this.thisPer.nombre, [ Validators.required, Validators.minLength(3), Validators.maxLength(75)]),
+      apellido: new FormControl(this.thisPer.apellido, [Validators.required, Validators.minLength(3), Validators.maxLength(75)]),
+      email: new FormControl( this.thisPer.email, [Validators.required, Validators.pattern(this.mailRegex)]),
+      fecha_nac: new FormControl(this.thisPer.fecha_nac, [Validators.required, Validators.maxLength(10)]),
+      titulo_actual: new FormControl(this.thisPer.titulo_actual, [Validators.required, Validators.maxLength(250)]),
+      sobre_mi: new FormControl(this.thisPer.sobre_mi, [Validators.required, Validators.maxLength(250)]),
+      contrasenia: new FormControl(this.thisPer.contrasenia, [Validators.required, Validators.maxLength(25)]),
+      linkedin: new FormControl(this.thisPer.linkedin, [Validators.required, Validators.maxLength(250), Validators.pattern(this.urlRegex)]),
+      facebook: new FormControl(this.thisPer.facebook, [Validators.required, Validators.maxLength(250),Validators.pattern(this.urlRegex)]),
+      github: new FormControl(this.thisPer.github, [Validators.required, Validators.maxLength(250), Validators.pattern(this.urlRegex)]),
+      id: new FormControl(this.thisPer.id, Validators.required)
+
     });
+
+    console.log(this.thisPer)
 
   }
 
