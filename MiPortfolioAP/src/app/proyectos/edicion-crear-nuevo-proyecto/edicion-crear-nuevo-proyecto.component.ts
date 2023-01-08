acd7219022e481a3c8cf4f5b3d9f36bc2db92a34
Leandro M <leoroan@gmail.com>
2023-01-08 13:41:06 -0300

    proeyctos component now can create new events

--- MiPortfolioAP/src/app/proyectos/edicion-crear-nuevo-proyecto/edicion-crear-nuevo-proyecto.component.ts
+++ MiPortfolioAP/src/app/proyectos/edicion-crear-nuevo-proyecto/edicion-crear-nuevo-proyecto.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-edicion-crear-nuevo-proyecto',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EdicionCrearNuevoProyectoComponent implements OnInit {
 
-  constructor() { }
+  someInput: any;
+  formdata: any;
+  urlRegex = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+
+  @Output() createEvent = new EventEmitter();
+
+  constructor(private datosPortfolio: PortfolioService) { }
 
   ngOnInit(): void {
+    this.formdata = new FormGroup({
+      nombre: new FormControl( '', [ Validators.required, Validators.minLength(10), Validators.maxLength(100)]),
+      url_foto: new FormControl('', [Validators.required, Validators.maxLength(250), Validators.pattern(this.urlRegex)]),
+      url_proyecto: new FormControl('', [Validators.required, Validators.maxLength(250)]),
+      descripcion: new FormControl('', [Validators.required, Validators.maxLength(250)])
+    });
+
   }
 
+  onClickSubmit(datos: any) {
+    this.datosPortfolio.agregarActualizarDatosProyectos(datos)
+      .subscribe(data => {
+        console.log(JSON.stringify(datos));
+        this.createEvent.emit();
+      })
+    this.formdata.reset();
+  }
 }
